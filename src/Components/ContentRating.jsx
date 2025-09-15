b67ce43e9f67ddca6aef36d9ec7145085cda347f
saramohamedfarag2005 <sara.m.farag2005@gmail.com>
2025-09-15 21:59:19 +0300

    Created a useState counter

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,18 +1,34 @@
+import { useState,useMemo } from "react";
+import "./ContentRating.css";
 
-import React, { Component } from 'react';
-import './ContentRating.css';
+export default function ContentRating(){
+const [likes, setLikes] = useState(0);
+const [dislikes, setDislikes] = useState(0);
 
-class ContentRating extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
-    );
-  }
+const totalRatings= useMemo(()=> likes +dislikes , [likes,dislikes]);
+const handelLikes= ()=> setLikes(l=>l+1);
+const handelDislikes = ()=> setDislikes(d=>d+1);
+
+return(
+
+  <div className="content-rating">
+    <p>
+    React was released by Facebook (now Meta) and popularized the
+        component-based approach for building UIs. Do you like this text?
+    </p>
+    <div className="rating-buttons">
+      <button className="rating-buttons" onClick={handelLikes}>
+        like ({likes})
+      </button>
+      <button className="dislike-button" onClick={handelDislikes}>
+        dislikes ({dislikes})
+
+      </button>
+
+      <p className="total" > Total ratings {totalRatings}</p>
+
+    </div>
+
+  </div>
+);
 }
-
-export default ContentRating;
